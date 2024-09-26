9ff9998e175005a0192662a655c41ab2064a2fef
Myles <114069097+panda74@users.noreply.github.com>
2024-09-26 23:37:07 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,11 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.panda74.fun": dockerHub,
+
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.panda74.fun": dockerHub,
 };
 
 function routeByHosts(host) {
